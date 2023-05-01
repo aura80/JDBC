7d5e07a2c6768deac1c3110a44e03ec120ba2be9
User <96388713+aura80@users.noreply.github.com>
2023-05-01 17:18:10 +0300

    Connections with methods and MysqlDataSource added

--- connectionsWithMethods/MethodsForConnection.java
+++ connectionsWithMethods/MethodsForConnection.java
@@ -2,22 +2,20 @@ package JDBC.connectionsWithMethods;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public abstract class MethodsForConnection {
 
     private static final String database = "jdbc_exercises";
-    private static final String username = "set_your_own_username";
-    private static final String password = "set_your_own_password";
+    private static final String username = "changeIt";    // de modificat pt pus pe GitHub
+    private static final String password = "changeIt";  // de modificat pt pus pe GitHub
 
     private static Connection connection;
 
     public static MysqlDataSource getMysqlDataSourceObject() {
         MysqlDataSource mysqlDataSource = new MysqlDataSource();
 
-        mysqlDataSource.setURL(String.format("jdbc:mysql://localhost:3306/%s",database));
+        mysqlDataSource.setURL(String.format("jdbc:mysql://localhost:3306/%s", database));
         mysqlDataSource.setUser(username);
         mysqlDataSource.setPassword(password);
 
@@ -25,16 +23,80 @@ public abstract class MethodsForConnection {
     }
 
     public static Connection getConnection() throws SQLException {
-        try{
+        try {
             connection = getMysqlDataSourceObject().getConnection();
             return connection;
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new SQLException(e);
         }
     }
 
     public static Statement getStatement() throws SQLException {
         return getConnection().createStatement();
+    }
+
+    public static void describeTable(String tableName) {
+        String describeTable = String.format("DESCRIBE TABLE %s", tableName);
+
+        // try with resources
+        try (Connection connection = getConnection();
+             PreparedStatement describeTableStatement = connection.prepareStatement(describeTable)) {
+
+            describeTableStatement.executeQuery();
+            System.out.println("Table was described successfully!");
+        } catch (SQLException e) {
+            System.out.println("Table was not described successfully due to: " + e.getMessage());
+        }
+    }
+
+    public static void insertInTable(String tableName, String studentName, String phone) {
+        String insertInTable = String.format("INSERT INTO %s (student_name, phone) VALUES ('%s', '%s')", tableName, studentName, phone);
+
+        // try with resources
+        try (Connection connection = getConnection();
+             PreparedStatement insertTableStatement = connection.prepareStatement(insertInTable)) {
+
+            insertTableStatement.executeUpdate();
+            System.out.println("Table was updated successfully!");
+        } catch (SQLException e) {
+            System.out.println("Table was not updated successfully due to: " + e.getMessage());
+        }
+    }
+
+    public static void deleteById(String tableName, int id) {
+        String deleteById = String.format("DELETE FROM %s WHERE id_student = ?", tableName);
+
+        try (Connection connection = getConnection();
+             PreparedStatement deleteByIdStatement = connection.prepareStatement(deleteById)) {
+
+            deleteByIdStatement.setInt(1, id);
+            deleteByIdStatement.executeUpdate();
+            System.out.println("Value was deleted by id successfully!");
+        } catch (SQLException e) {
+            System.out.println("Value was not deleted successfully by id due to: " + e.getMessage());
+        }
+    }
+
+    public static void showTable(String tableName) {
+        String selectFromTable = String.format("SELECT * FROM %s", tableName);
+
+        // try with resources
+        try (Connection connection = getConnection();
+             PreparedStatement selectFromTableStatement = connection.prepareStatement(selectFromTable)) {
+
+            System.out.println("-----------START OF THE TABLE-----------");
+            ResultSet resultSet = selectFromTableStatement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("student_name");
+                String phone = resultSet.getString("phone");
+                int id = resultSet.getInt("id_student");
+                System.out.println("Student: " + name + ",  Phone: " + phone + ",  Id: " + id);
+            }
+        } catch (SQLException e) {
+            System.out.println("Table was not shown successfully due to: " + e.getMessage());
+        } finally {
+            System.out.println("-----------END OF THE TABLE-----------");
+        }
     }
 
     public static void closeConnection() throws SQLException {
--- connectionsWithMethods/QueryExecutionAndMapping.java
+++ connectionsWithMethods/QueryExecutionAndMapping.java
@@ -4,6 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class QueryExecutionAndMapping {
+
+    private static final String tableName = "student";
+
     public static void main(String[] args) throws SQLException {
 
         ResultSet rs = MethodsForConnection.getStatement().executeQuery("select * from student where id_student=4");
@@ -14,6 +17,8 @@ public class QueryExecutionAndMapping {
             System.out.println("Student: " + name + ",  Phone: " + phone + ",  Id: " + id);
         }
 
+        System.out.println("----------------------------------------------");
+
         ResultSet rs1 = MethodsForConnection.getStatement().executeQuery("select * from student where id_student=3");
         while (rs1.next()) {
             String name = rs1.getString("student_name");
@@ -21,6 +26,8 @@ public class QueryExecutionAndMapping {
             int id = rs1.getInt("id_student");
             System.out.println("Student: " + name + ",  Phone: " + phone + ",  Id: " + id);
         }
+
+        System.out.println("----------------------------------------------");
 
         ResultSet rs2 = MethodsForConnection.getStatement().executeQuery("select * from student where id_student=6");
         while (rs2.next()) {
@@ -30,6 +37,8 @@ public class QueryExecutionAndMapping {
             System.out.println("Student: " + name + ",  Phone: " + phone + ",  Id: " + id);
         }
 
+        System.out.println("----------------------------------------------");
+
         ResultSet rs3 = MethodsForConnection.getStatement().executeQuery("select * from student where id_student<=3");
         while (rs3.next()) {
             String name = rs3.getString("student_name");
@@ -37,7 +46,41 @@ public class QueryExecutionAndMapping {
             int id = rs3.getInt("id_student");
             System.out.println("Student: " + name + ",  Phone: " + phone + ",  Id: " + id);
         }
-        
+
+        System.out.println("----------------------------------------------");
+
+        ResultSet rs4 = MethodsForConnection.getStatement().executeQuery("select * from student");
+        while (rs4.next()) {
+            String name = rs4.getString("student_name");
+            String phone = rs4.getString("phone");
+            int id = rs4.getInt("id_student");
+            System.out.println("Student: " + name + ",  Phone: " + phone + ",  Id: " + id);
+        }
+
+        System.out.println("----------------------------------------------");
+
+        MethodsForConnection.describeTable(tableName);
+
+        System.out.println("----------------------------------------------");
+
+//        must be renewed its arguments at every new run
+
+//        MethodsForConnection.insertInTable(tableName, "Ica", "0735418725");
+//        MethodsForConnection.insertInTable(tableName, "Zora", "0745687123");
+//        MethodsForConnection.insertInTable(tableName, "Elena", "0747159357");
+
+//        MethodsForConnection.insertInTable(tableName, "Mariana", "0735418725");
+//        MethodsForConnection.insertInTable(tableName, "Dariana", "0745687123");
+//        MethodsForConnection.insertInTable(tableName, "Dorina", "0747159357");
+
+        System.out.println("----------------------------------------------");
+
+        MethodsForConnection.deleteById(tableName, 35);
+        MethodsForConnection.deleteById(tableName, 34);
+        MethodsForConnection.deleteById(tableName, 39);
+
+        MethodsForConnection.showTable(tableName);
+
         MethodsForConnection.closeConnection();
 
     }
