f910742ca42a1b292e995400d672f8f5b512d583
User <96388713+aura80@users.noreply.github.com>
2023-05-02 01:49:42 +0300

    .sql file added

--- jdbc_exercises_ddl.sql
+++ jdbc_exercises_ddl.sql
@@ -1,11 +1,11 @@
 create database jdbc_exercises;
 
-create user student identified by 'student_password';
+--create user student identified by 'student_password';
 
 use jdbc_exercises;
 
 create table student (
-	id_student int not null auto_increment primary key,
+    id_student int not null auto_increment primary key,
     student_name varchar(150) not null unique,
     phone varchar(10) not null
 );
@@ -13,7 +13,7 @@ create table student (
 describe student;
 
 create table school (
-	id_school int not null primary key,
+    id_school int not null primary key,
     school_name varchar(50),
     id_student int,
     CONSTRAINT kf_schools_students FOREIGN KEY (id_student) REFERENCES student(id_student)
@@ -22,13 +22,13 @@ create table school (
 describe schools;
 
 insert into student(student_name, phone) values ("Gica", "0722745896"), ("Aurel","0724561146"), ("Costel","0789145236"),
-												("Mihaita","0247265148"), ("Anatol","0554799145"), ("Azorel","0245689745");
+                                                ("Mihaita","0247265148"), ("Anatol","0554799145"), ("Azorel","0245689745");
                                                 
 select * from student;
 
 insert into school(id_school, school_name, id_student) values (22, "Liceul Mihai Viteazu", 4),(28, "Ana Aslan", 3),
-															   (15, "I.L. Caragiale", 2),(8, "Aurel Vlaicu", 5),
-                                                               (32, "Titu Maiorescu", 1),(20, "I.L. Caragiale" , 6);                                    
+                                                              (15, "I.L. Caragiale", 2),(8, "Aurel Vlaicu", 5),
+                                                              (32, "Titu Maiorescu", 1),(20, "I.L. Caragiale" , 6);
 
 select * from school;
 
